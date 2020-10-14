4f9e0b54245ca1c12ca98fdeec4bcc3879b7718b
Kazuki3dayo <fate0007rx7@yahoo.co.jp>
2020-10-14 20:55:16 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true #バリデーション設定。登録時に空欄だとエラーが発生する。
 end
