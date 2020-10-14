425830a47f91e8658928a74f9c3c0ceb317291ed
Kazuki3dayo <fate0007rx7@yahoo.co.jp>
2020-10-14 20:25:24 +0900

    ユーザー変種機能の実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+
+  def edit
+  end
+
+  def update  #updateアクション内で、保存できた場合とできなかった場合で条件分岐の処理を行います。
+    if current_user.update(user_params)  #ユーザー情報が格納されているcurrent_userメソッドを使用して、ログインしているユーザーの情報を更新する。
+      redirect_to root_path #current_user.updateに成功した場合、root（チャット画面）にリダイレクトします。
+    else  #失敗した場合、render :editとeditのアクションを指定しているため、editのビューが表示されます。
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params #ストロングパラメーターを使用し、user_paramsを定義
+    params.require(:user).permit(:name, :email) #permitメソッドを使用し、「name」と「email」の編集を許可する。
+  end
+
+end
